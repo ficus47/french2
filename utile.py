f77587aab86166e693e33587676ef33addbcdb39
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-01-17 17:19:43 +0100

    Update utile.py

--- utile.py
+++ utile.py
@@ -12,8 +12,8 @@ def valid(a,b,d):
 
     text = ""
     text2 = ""
-    write(Google.search(a, language="fr", number_of_results=d))
-    for i in Google.search(a, language="fr", number_of_results=d):
+    write(Google.search(a, number_of_results=d))
+    for i in Google.search(a, lnumber_of_results=d):
       if b not in i.description or b == "" or b == " ":
         text += i.description if i.description != "" or i.description != " " else "aucune description trouvée ;("
         text += "  :  "
