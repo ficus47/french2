000e34689bcc1e35bc19814b2d7139fab62411cc
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-01-17 17:47:11 +0100

    Update utile.py

--- utile.py
+++ utile.py
@@ -1,7 +1,14 @@
 from streamlit import write
 from googlesearcher import *
-import googlesearcher
 import inspect
+
+try:
+  from googlesearch import Search
+  
+except Exception:
+  write(inspect.signature(Search.__init__).parameters + "e")
+
+write(inspect.signature(Search.__init__).parameters)
 
 
 def valid(a,b,d):
@@ -12,28 +19,27 @@ def valid(a,b,d):
 
     text = ""
     text2 = ""
-
-    for i in Google.search(a, num=str(d)):
-      if i.title is not None:
-        if b not in i.title or b == "" or b == " ":
-          text += i.title if i.title != "" or i.title != " " else "aucune description trouvée ;("
-      else:
-        text += "aucune description trouvée ;("
+    write(Search(a, num=str(d)))
+    for i in Search(a, num=str(d)):
+      if b not in i.description or b == "" or b == " ":
+        text += i.title if i.title != "" or i.title != " " else "aucune description trouvée ;("
         text += "  :  "
-        text2 += "~"
-        text2 += i.link
-        text2 += "\n\n" 
+        text += "\n\n"
+        text += i.link
+        text += "\n\n" 
 
 
 
-    je = ""
-    for i, o in zip(textwrap.wrap(text=text,width=500,break_long_words=False), text2.split("~")):
-      je += i[:60 if len(i) > 60 else len(i)] + "..."if len(i) > 60 else " "
-      je += "  :  "
-      je += o
-      je += "\n\n"
+    #je = ""
+    #for i, o in zip(textwrap.wrap(text=text,width=500,break_long_words=False), text2.split("~")):
+    #  je += i[:60 if len(i) > 60 else len(i)] + "..."if len(i) > 60 else " "
+    #  je += "  :  "
+    #  je += o
+    #  je += "\n\n"
+#
+#
+#
+    return text
 
 
-    print(je)
-    return je
 
