d38abcd612fa7eed9525b71e3130fdbc8fe45c0a
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-01-17 17:49:09 +0100

    Update utile.py

--- utile.py
+++ utile.py
@@ -2,13 +2,6 @@ from streamlit import write
 from googlesearcher import *
 import inspect
 
-try:
-  from googlesearch import Search
-  
-except Exception:
-  write(inspect.signature(Search.__init__).parameters + "e")
-
-write(inspect.signature(Search.__init__).parameters)
 
 
 def valid(a,b,d):
@@ -18,10 +11,10 @@ def valid(a,b,d):
 
 
     text = ""
-    text2 = ""
+
     write(Search(a, num=str(d)))
     for i in Search(a, num=str(d)):
-      if b not in i.description or b == "" or b == " ":
+      if b not in i.title or b == "" or b == " ":
         text += i.title if i.title != "" or i.title != " " else "aucune description trouvée ;("
         text += "  :  "
         text += "\n\n"
