db189fe1cb8d44882572e9dff1bcc6f13d0115e0
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-02-28 18:24:10 +0000

    19

--- Hello.py
+++ Hello.py
@@ -37,21 +37,21 @@ st.button("autre chose de fun .", on_click=lambda:st.snow())
 st.button("derniere chose de fun .", on_click=lambda:b())
 st2 = st.container()
 
-a = valid(mots, ban_words, number, extent)
 
 def clicker():
+  pass
+  st2.Write(valid(mots, ban_words, number, extent))
+  #try:
+    #st2.write(a[1])
+  #except Exception:
+    #pass
+    #st2.write(a[0])
   
-  try:
-    st2.write(a[1])
-  except Exception:
-    pass
-    st2.write(a[0])
-  
-  c = a[0].split("+++++++")
+  #c = a[0].split("+++++++")
 
   
 
-  for i in c:
-    st2.write(i)
+  #for i in c:
+    #st2.write(i)
 
-st.button("entrez .", on_click=clicker())
+st2.button("entrez .", on_click=clicker())
